0680e96d61ee4a52fd53e5eb93a3b29e952e6152
Андрей Замятин <andreizamyat1n@yandex.ru>
2024-02-26 22:05:43 +0500

    Самостоятельная работа

--- СМ_1/СМ_1.cpp
+++ СМ_1/СМ_1.cpp
@@ -1,7 +1,54 @@
 ﻿#include <iostream>
+#include <string>
+using namespace std;
 
-int main()
+class Animal 
 {
-    
-}
+protected:
+    string name;
+    int age;
 
+public:
+    Animal(string n, int a) : name(n), age(a) {}
+
+    virtual void makeSound() 
+    {
+        cout << "Animal makes a sound" << endl;
+    }
+};
+
+class Dog : public Animal 
+{
+public:
+    Dog(string n, int a) : Animal(n, a) {}
+
+    void makeSound() override 
+    {
+        cout << "Dog barks" << endl;
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    Cat(string n, int a) : Animal(n, a) {}
+
+    void makeSound() override 
+    {
+        cout << "Cat meows" << endl;
+    }
+};
+
+int main() 
+{
+    Animal animal1("Bobr", 3);
+    animal1.makeSound(); // Вывод: Animal makes a sound
+
+    Dog dog1("Star", 5);
+    dog1.makeSound(); // Вывод: Dog barks
+
+    Cat Cat1("Star", 5);
+    Cat1.makeSound(); // Вывод: Dog barks
+
+    return 0;
+}
